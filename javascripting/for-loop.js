81ebbb505ee6a28c995f69981903e61192f614c9
pmckeegan <pmckeegan@gmail.com>
2019-02-16 15:40:20 +0000

    Initial commit with all of my work so far

---
+++ javascripting/for-loop.js
@@ -0,0 +1,10 @@
+//for loops
+
+var total = 0;
+var limit = 10;
+for (var i = 0; i < limit; i++)
+{ total += i;
+    //run the total til it reaches the limit
+}
+
+console.log(total)
---
+++ javascripting/if-statement.js
@@ -0,0 +1,14 @@
+//fruit
+
+var fruit = 'Orange';
+
+if (fruit > 5)
+{
+    console.log ('The fruit name has five characters or less.');
+}
+
+else
+    {
+        console.log ('The fruit name has more than five characters.')
+    }
+
