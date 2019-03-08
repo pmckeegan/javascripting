bf98db758fa5cf5344026da46c5374a4bb0d1b54
Patrick McKeegan <47393718+pmckeegan@users.noreply.github.com>
2019-03-08 11:53:20 -0500

    Delete index.js

--- w1d5/index.js
+++
@@ -1,4 +0,0 @@
-var chalk = require("chalk");
-
-var message = chalk.bgRed.red("Well this is mysterious! " + chalk.bgGreen("This is a bit better..... \n"))  + chalk.blue("red, \n") + chalk.red("blue, \n ") + chalk.cyan("What happened there!?");
-console.log(message);
